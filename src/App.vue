a3a8079654d405a2ce424d1478a4032baf6ed468
wuliliwu <15219295349@163.com>
2020-09-25 11:01:06 +0800

    '2020-9-25'

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
 
 <style>
   /*导入需要初始化的css样式，两个/base/normaliza*/
-  @import "./assets/css/base.css";
-  @import "./assets/css/normalize.css";
+  @import "assets/css/base.css";
+  @import "assets/css/normalize.css";
 
 </style>
